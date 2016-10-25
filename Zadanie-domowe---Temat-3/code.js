ae8b90dd92277056e8e25a26a9dac746e11fdf07
aresblade <aresblade18@gmail.com>
2016-10-25 22:46:51 +0200

    Update code.js

--- Zadanie-domowe---Temat-3/code.js
+++ Zadanie-domowe---Temat-3/code.js
@@ -6,27 +6,21 @@
 	}
     
     global.UAM.library = []; 
-
     global.UAM.addCategory = function (newCategoryName) {
 		global.UAM.library.push({
 		categoryName: newCategoryName,
 		books: []
      });
     };
-
-
+	
     global.UAM.removeCategory = function (categoryObject) {
-			for (i = 0; i < UAM.library.length; i++) { 
-					if (UAM.library[i].categoryName == categoryObject){
-						delete global.UAM.library[i];
-					}
+		for (i = 0; i < UAM.library.length; i++) { 
+			if (UAM.library[i].categoryName == categoryObject){
+				delete global.UAM.library[i];
 			}
-			
+		}	
     };
-
-
     global.UAM.addBookToCategory = function(categoryObject, title1, count1) {
-
 		for (i = 0; i < UAM.library.length; i++) { 
 			if (UAM.library[i].categoryName == categoryObject){		
 				global.UAM.library[i].books.push({
@@ -40,66 +34,54 @@
 
     global.UAM.removeBookFromCategory = function(categoryObject, bookObject) {
 		for (i = 0; i < UAM.library.length; i++) { 
-					if (UAM.library[i].categoryName == categoryObject){
-						for (j = 0; j < UAM.library[i].books.length; j++) { 
-							if (UAM.library[i].books[j].title == bookObject){
-								delete global.UAM.library[i].books[j];
-							}
-						}
+			if (UAM.library[i].categoryName == categoryObject){
+				for (j = 0; j < UAM.library[i].books.length; j++) { 
+					if (UAM.library[i].books[j].title == bookObject){
+						delete global.UAM.library[i].books[j];
 					}
+				}
 			}
-		
+		}
     };
 
 
     global.UAM.getCategoryByName = function(categoryName1) {
-
 		for (i = 0; i < UAM.library.length; i++) { 
-					if (UAM.library[i].categoryName == categoryName1){
-						return global.UAM.library[i];
-					}
+			if (UAM.library[i].categoryName == categoryName1){
+				return global.UAM.library[i];
 			}
-		
-		
+		}
     };
-
 
     global.UAM.getBookByTitle = function(title1) {
-
 		for (i = 0; i < UAM.library.length; i++) { 
-						for (j = 0; j < UAM.library[i].books.length; j++) { 
-							if (UAM.library[i].books[j].title == title1){
-								return global.UAM.library[i].books[j];
-							}
-						}
-					
+			for (j = 0; j < UAM.library[i].books.length; j++) { 
+				if (UAM.library[i].books[j].title == title1){
+					return global.UAM.library[i].books[j];
+				}
 			}
+		}
     };
-
 
     global.UAM.returnBook = function(bookObject) {
         for (i = 0; i < UAM.library.length; i++) { 
-						for (j = 0; j < UAM.library[i].books.length; j++) { 
-							if (UAM.library[i].books[j].title == bookObject){
-							   global.UAM.library[i].books[j].count++;
-							}
-						}
-					
-			}
+			for (j = 0; j < UAM.library[i].books.length; j++) { 
+				if (UAM.library[i].books[j].title == bookObject){
+				   global.UAM.library[i].books[j].count++;
+				}
+			}		
+		}
     };
     
     global.UAM.getBorrowedBooks = function() {
 		var array = [];
 		var count = 0;
-		 for (i = 0; i < UAM.library.length; i++) { 
-						for (j = 0; j < UAM.library[i].books.length; j++) { 
-							
-							  array[count] =  global.UAM.library[i].books[j];
-							  count++;
-						}
-					
-			}
-			return array;
+		for (i = 0; i < UAM.library.length; i++) { 
+			for (j = 0; j < UAM.library[i].books.length; j++) { 
+				array[count] =  global.UAM.library[i].books[j];
+				count++;
+			}	
+		}
+		return array;
     };
-
 }(window));
