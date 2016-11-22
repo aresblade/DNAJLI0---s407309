c13bd4d1af0d864d88a6c2afdecf5efb87488fbd
aresblade <aresblade18@gmail.com>
2016-11-22 23:03:23 +0100

    Add files via upload

--- Temat 5/homework2/index.js
+++ Temat 5/homework2/index.js
@@ -1,51 +1,87 @@
-function templatka(Obj)
-{
-	return "<div>"+
-			 Obj + "    " +
-			 "<button id='usun'> - </button>"+
-			 "<input type= 'checkbox' id ='check' />"+
-			"</div>";
+//Ta śmieszna funkcja dodaje tzw. listy, tworzy checkboxa oraz ten drugi śmieszny pliczek
+function getTemplate(Obj) {
+    return  "<div class='list-row' style=' display: inline-block;'>" + 
+						 Obj + 
+					"</div>" +
+				"<input type='checkbox' id='myCheck'>" +
+            "<button class='List-remove'>-</div>";
+}
+//Na początku był tu obiekt, ale teraz nie ma znaczenia skoro jest to jeden argument, bezużyteczne na ten moment
+function readList() {
+ 	   var ListValue = document.querySelector('#List');
+	   
+    return {
+        List: ListValue.value,
+    };
 }
 
 
-	var list =[];
-	var click = document.getElementById("Add");
-	click.addEventListener('click', function() {
-		var dodaj = document.getElementById("Historyjka").value;
+//To jest ważne
+window.addEventListener('DOMContentLoaded', function () {
+	
+		var zlicz = 0;
+		var max = 0;
+		 document.getElementById("max").innerHTML = max;	// wypisuje od razu ile jest max elementow
+		  document.getElementById("ilosc").innerHTML = zlicz; // Wypisuje od razu ile jest obecnie zaznaczonych elementow
+		var AddToListBtn = document.querySelector('#AddToList');	// Biere buttona do selectora
 		
-		var gdziemabyc = document.getElementById("tutaj");
-		var template = templatka(dodaj);
-		var DOM = document.createElement("li");
-		list.push(dodaj);
-		DOM.innerHTML = template;
-		gdziemabyc.appendChild(DOM);
-		
-		var usun = DOM.querySelector("#usun");
-		usun.addEventListener('click', function() {
+	    AddToListBtn.addEventListener('click', function() {	// tworze z tym smiesznym buttonem akcje
+			 
+			 
+			   var list = [];	// Bartek, to tworzy tablice
+			   
+			   var ListValue = document.querySelector('#List').value;	// tutaj pobieram wartosc z pola textowego
 			
-			var index = list.indexOf(dodaj);
-			if(index >=0)
-			{
-				list.splice(index,1);
-			}
-			tutaj.removeChild(DOM);
-		});
-		
-		var checkbox = DOM.querySelector("#check");
-		checkbox.addEventListener('change', function() {
-			if (checkbox.checked)
-			{
-			var gdzie=	document.getElementById("zaznaczone");
-			gdzie.innerHTML = "Zaznaczyłeś mnie";
-			}
-			else
-			{
-			document.getElementById("zaznaczone").innerHTML= "Odznaczyłeś mnie";
-			}
+				var Listlist = document.querySelector('#Listlist');	
 			
-		});
+				var template = getTemplate(ListValue);	// Proste przypisanie wartosci z textu do templatki
+				var DomElement = document.createElement('li'); // nie mam pojęcia po co to jest, ale działa więc niech będzie, 
+				list.push(ListValue);	
+				DomElement.innerHTML = template;	
+				if (ListValue.length > 0)	// sprawdzam czy dlugosc tekstu jest wieksza niz 0
+				{
+				max++;
+				document.getElementById("max").innerHTML = max;	// po zwiekszeniu wypisuje
+				Listlist.appendChild(DomElement);	// wrzucam cala templatke
+				}
+				
+				
+				var removeBtn = DomElement.querySelector('.List-remove');	//tworzymy buttona z -
+				removeBtn.addEventListener('click', function() {
+				
+					var index = list.indexOf(ListValue);	// pobieram indexa tego elementu ktory sobie zaznacze // chyba
+					if (index >= 0) {
+						list.splice(index, 1);
+					}
+					
+					Listlist.removeChild(DomElement);	// wiadomo	
+				      max--;
+					 document.getElementById("max").innerHTML = max;	
+					if ( Checkbox.checked && zlicz > 0)	
+					{
+					 zlicz--;
+					 document.getElementById("ilosc").innerHTML = zlicz;
+					}
+				
+				});
+				
+				var Checkbox = DomElement.querySelector('#myCheck');	//to samo co wyzej
+				Checkbox.addEventListener('change', function() {
+				if (Checkbox.checked) {
+					zlicz++;
+					document.getElementById("ilosc").innerHTML = zlicz;
+				} else {
+					zlicz--;
+					 document.getElementById("ilosc").innerHTML = zlicz;
+				}
+			});
 		
-	});
+		
+    });
+	
+
 	
 	
 	
+	
+});
